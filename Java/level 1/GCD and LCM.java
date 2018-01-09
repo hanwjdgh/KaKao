06b2aa7e6fdb970defebf413cdb8e307b0a4aa27
hanwjdgh <hanwjdgh@naver.com>
2018-01-10 08:03:03 +0900

    level 1 - GCD and LCM

---
+++ Java/level 1/GCD and LCM.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+class TryHelloWorld {
+ public int[] gcdlcm(int a, int b) {
+        int[] answer = new int[2];
+
+        answer[0] = gcd(a,b);
+        answer[1] = (a*b)/answer[0];
+        return answer;
+    }
+
+   public static int gcd(int p, int q){
+    if (q == 0) return p;
+    return gcd(q, p%q);
+   }
+
+    // 아래는 테스트로 출력해 보기 위한 코드입니다.
+    public static void main(String[] args) {
+        TryHelloWorld c = new TryHelloWorld();
+        System.out.println(Arrays.toString(c.gcdlcm(3, 12)));
+    }
+}
