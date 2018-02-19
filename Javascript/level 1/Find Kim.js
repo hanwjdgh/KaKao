d41ccbbbcb576ae0a2fd94d3c08f1170aa173557
hanwjdgh <hanwjdgh@naver.com>
2018-02-20 08:11:45 +0900

    level 1 - Find Kim

---
+++ Javascript/level 1/Find Kim.js
@@ -0,0 +1,8 @@
+function findKim(seoul){
+  var idx = seoul.indexOf("Kim");
+  //함수를 완성하세요
+  return "김서방은 " + idx + "에 있다";
+}
+
+// 실행을 위한 테스트코드입니다.
+console.log( findKim(["Queen", "Tod", "Kim"]));
