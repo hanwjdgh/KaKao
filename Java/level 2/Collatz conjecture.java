969d7fb2418d99b63307fe9528990883008fa6eb
hanwjdgh <hanwjdgh@naver.com>
2018-03-06 08:10:33 +0900

    level 2 - Collatz conjecture

---
+++ Java/level 2/Collatz conjecture.java
@@ -0,0 +1,25 @@
+class Collatz {
+	public int collatz(long num) {
+		int answer = 0;
+		while(num>1){
+    	if(num%2==0)
+     		 num/=2;
+    	else 
+     		 num=(num*3)+1;
+    	answer++;
+    
+    	if(answer==500){
+      	answer=-1;
+   		 	break;
+  		}
+  	}
+		return answer;
+	}
+
+	// 아래는 테스트로 출력해 보기 위한 코드입니다.
+	public static void main(String[] args) {
+		Collatz c = new Collatz();
+		int ex = 1117065;
+		System.out.println(c.collatz(ex));
+	}
+}
