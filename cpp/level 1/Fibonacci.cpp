7649028c2d19b47cd465f163ee2512478910b8d5
hanwjdgh <hanwjdgh@naver.com>
2018-06-15 00:45:40 +0900

    cpp level 1 - Fibonacci

---
+++ cpp/level 1/Fibonacci.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+     int answer = 0;
+     int a = 0;
+     int b = 1;
+     for(int i=0; i<n; i++) {
+          if(i != 0){
+              answer = a + b;
+              a = b%1234567;
+              b = answer%1234567;
+          }
+      }
+      return answer;
+}
