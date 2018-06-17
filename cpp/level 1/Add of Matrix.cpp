a088d2394a7ae3ba177c12e1f2e6974c0ffe1d7c
hanwjdgh <hanwjdgh@naver.com>
2018-06-17 09:05:05 +0900

    cpp level 1 - Add of Matrix

--- cpp/level 1/Add of Matrix.cpp
+++ cpp/level 1/Add of Matrix.cpp
@@ -8,15 +8,15 @@ vector<vector<int>> solution(vector<vector<int>> arr1, vector<vector<int>> arr2)
 
     int temp=0;
     for(int a=0; a<arr1.size(); a++){
-    vector<int> tem;
-    for(int b=0; b<arr2[0].size();b++){
-      temp=0;
-      for(int c=0; c<arr2.size();c++){
-        temp+=(arr1[a][c]*arr2[c][b]);
+      vector<int> tem;
+      for(int b=0; b<arr2[0].size();b++){
+        temp=0;
+        for(int c=0; c<arr2.size();c++){
+          temp+=(arr1[a][c]*arr2[c][b]);
+        }
+        tem.push_back(temp);
       }
-      tem.push_back(temp);
-     }
-     answer.push_back(tem);
+      answer.push_back(tem);
     }
     return answer;
 }
