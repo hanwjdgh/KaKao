fbf038ac10a8218a716e5639c1ae3c4594d6a9e9
hanwjdgh <hanwjdgh@naver.com>
2018-06-18 12:00:42 +0900

    python level 1 - I hate the same number

--- Python/level 1/I hate the same number.py
+++ Python/level 1/I hate the same number.py
@@ -1,15 +1,19 @@
-def no_continuous(s):
-    # 함수를 완성하세요
+def solution(arr):
     answer = []
     tem =""
-    for a in range(len(s)):
+    for a in range(len(arr)):
         if a==0:
-            answer.append(s[a])
-            tem = s[a]
-        elif tem != s[a]:
-            answer.append(s[a])
-            tem = s[a]
+            answer.append(arr[a])
+            tem = arr[a]
+        elif tem != arr[a]:
+            answer.append(arr[a])
+            tem = arr[a]
     return answer
-
-# 아래는 테스트로 출력해 보기 위한 코드입니다.
-print( no_continuous( "133303" ))
+'''
+def no_continuous(s):
+    a = []
+    for i in s:
+        if a[-1:] == [i]: continue
+        a.append(i)
+    return a
+'''
