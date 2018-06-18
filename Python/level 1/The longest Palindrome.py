6e91194d803f7df207f9b78d1f3bf3b5d45be20f
hanwjdgh <hanwjdgh@naver.com>
2018-06-18 11:57:03 +0900

    python level 1 - The longest Palindrome

--- Python/level 1/The longest Palindrome.py
+++ Python/level 1/The longest Palindrome.py
@@ -1,4 +1,4 @@
-def longest_palindrom(s):
+def solution(s):
     result = []
     for i in range(0,len(s)):
         for j in range(1,len(s)+1):
@@ -8,7 +8,4 @@ def longest_palindrom(s):
                 result.append(len(s[j:i]))
     return max(result)
 
-
-# 아래는 테스트로 출력해 보기 위한 코드입니다.
-print(longest_palindrom("토마토맛토마토"))
-print(longest_palindrom("토마토맛있어"))
+#69.3
