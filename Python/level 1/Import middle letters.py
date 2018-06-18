ef31bd3530b4b6070699c2ee23266b9fe69ca948
hanwjdgh <hanwjdgh@naver.com>
2018-06-18 11:52:16 +0900

    python level 1 - Import middle letters

--- Python/level 1/Import middle letters.py
+++ Python/level 1/Import middle letters.py
@@ -1,10 +1,6 @@
-def string_middle(str):
-    # 함수를 완성하세요
-    tlen = len(str)
+def solution(s):
+    tlen = len(s)
     if tlen % 2 == 1:
-        return str[tlen//2]
+        return s[tlen//2]
     else:
-        return str[tlen//2-1]+str[tlen//2]
-
-# 아래는 테스트로 출력해 보기 위한 코드입니다.
-print(string_middle("power"))
+        return s[tlen//2-1]+s[tlen//2]
--- Python/level 2/2016.py
+++
@@ -1,7 +0,0 @@
-def getDayName(a,b):
-    mon = [31,29,31,30,31,30,31,31,30,31,30,31]
-    day = ["FRI","SAT","SUN","MON","TUE","WED","THU"]
-    return day[((sum(mon[0:a-1])+b)%7)-1]
-
-#아래 코드는 테스트를 위한 출력 코드입니다.
-print(getDayName(5,24))
