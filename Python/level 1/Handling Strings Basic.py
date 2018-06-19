4fda58bd7370fad02328016caae3ca38a48e1488
hanwjdgh <hanwjdgh@naver.com>
2018-06-20 08:20:34 +0900

    python level 1 - Handling Strings Basic

--- Python/level 1/Handling Strings Basic.py
+++ Python/level 1/Handling Strings Basic.py
@@ -1,10 +1,4 @@
-function alpha_string46(s){
-  // 함수를 완성하세요
-	if((s.length === 4 || s.length === 6) && !isNaN(s))
-  	return true;
-  return false;
-}
-
-
-// 아래는 테스트로 출력해 보기 위한 코드입니다.
-console.log( alpha_string46("a234") );
+def solution(s):
+    if (len(s) == 4 or len(s) == 6) and s.isdigit():
+        return True
+    return False
