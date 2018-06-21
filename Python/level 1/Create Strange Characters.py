f8d442fc9c963aa7d95f76603da7cc942446e8ff
hanwjdgh <hanwjdgh@naver.com>
2018-06-22 08:07:26 +0900

    python level 1 - Create Strange Characters

--- Python/level 1/Create Strange Characters.py
+++ Python/level 1/Create Strange Characters.py
@@ -1,5 +1,4 @@
-def toWeirdCase(s):
-    # 함수를 완성하세요
+def solution(s):
     list = s.split(" ")
     list2 = []
     for str in list:
@@ -12,6 +11,3 @@ def toWeirdCase(s):
         list2.append(answer)
     
     return " ".join(list2)
-
-# 아래는 테스트로 출력해 보기 위한 코드입니다.
-print("결과 : {}".format(toWeirdCase("try hello world")));
