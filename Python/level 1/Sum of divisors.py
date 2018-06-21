2d12f39a24cde958cdc411da02d2745f7990b77f
hanwjdgh <hanwjdgh@naver.com>
2018-06-22 08:06:01 +0900

    python level 1 - Sum of divisors

--- Python/level 1/Sum of divisors.py
+++ Python/level 1/Sum of divisors.py
@@ -1,9 +1,6 @@
-def sumDivisor(num):
+def solution(n):
     answer = 0
-    for a in range(1,num+1):
-        if num % a ==0:
+    for a in range(1,n+1):
+        if n % a ==0:
             answer+=a
     return answer
-
-# 아래는 테스트로 출력해 보기 위한 코드입니다.
-print(sumDivisor(12))
