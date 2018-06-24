95afbdb595dfec8151efbf82470d11d105d453a9
hanwjdgh <hanwjdgh@naver.com>
2018-06-25 08:34:51 +0900

    python level 1 - Determining an integer root square

--- Python/level 1/Determining an integer root square.py
+++ Python/level 1/Determining an integer root square.py
@@ -1,10 +1,6 @@
 import math
-def nextSqure(n):
-    # 함수를 완성하세요
+def solution(n):
     tem = math.sqrt(n)
     if int(tem) == tem:
         return int(pow(tem+1,2))
-    return 'no'
-
-# 아래는 테스트로 출력해 보기 위한 코드입니다.
-print("결과 : {}".format(nextSqure(121)));
+    return -1
