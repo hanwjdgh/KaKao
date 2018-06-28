be3478cfc92c1403f118074f256c6b3fa9c21d70
hanwjdgh <hanwjdgh@naver.com>
2018-06-29 08:16:33 +0900

    python level 1 - n numbers spaced by x

--- Python/level 1/n numbers spaced by x.py
+++ Python/level 1/n numbers spaced by x.py
@@ -1,11 +1,7 @@
-def number_generator(x, n):
-    # 함수를 완성하세요
+def solution(x, n):
     answer=[]
     tem=x
     for a in range(0,n):
         answer.append(x)
         x+=tem
     return answer
-
-# 아래는 테스트로 출력해 보기 위한 코드입니다.
-print(number_generator(3,5))
