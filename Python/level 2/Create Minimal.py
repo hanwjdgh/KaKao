1a0a5fdc4f72b072681c26c7c13ff66f8e8af748
hanwjdgh <hanwjdgh@naver.com>
2018-07-03 08:03:53 +0900

    python level 2 - Create Minimal

--- Python/level 2/Create Minimal.py
+++ Python/level 2/Create Minimal.py
@@ -1,4 +1,4 @@
-def getMinSum(A,B):
+def solution(A,B):
     answer = 0
     A.sort()
     B.sort()
@@ -6,7 +6,3 @@ def getMinSum(A,B):
     for i,j in zip(A,B):
         answer+=i*j
     return answer
-
-#아래 코드는 출력을 위한 테스트 코드입니다.
-
-print(getMinSum([1,2],[3,4]))
