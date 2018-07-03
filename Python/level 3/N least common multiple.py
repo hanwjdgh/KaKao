524f43dde9bc8b0633924055282e2cb037dd7650
hanwjdgh <hanwjdgh@naver.com>
2018-07-04 08:03:04 +0900

    python level 2 - N least common multiple

--- Python/level 3/N least common multiple.py
+++ Python/level 3/N least common multiple.py
@@ -9,9 +9,6 @@ def gcd(a, b):
 def lcm(a,b):
     return a * b / gcd(a, b)
 
-def nlcm(num):
+def solution(num):
     num.sort()
     return reduce(lcm,num)
-
-# 아래는 테스트로 출력해 보기 위한 코드입니다.
-print(nlcm([2,6,8,14]));
