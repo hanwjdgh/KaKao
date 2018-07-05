f81dd1bf02a3d945a9b24d8fa48fefd632695814
hanwjdgh <hanwjdgh@naver.com>
2018-07-05 17:04:45 +0900

    python level 2 - Find the largest square

--- Python/level 2/Find the largest square.py
+++ Python/level 2/Find the largest square.py
@@ -1,8 +1,13 @@
-def findLargestSquare(board):
+def solution(board):
     answer = 0
+    if(len(board)==1):
+        if(board[0][0]==0):
+            return 0
+        else:
+            return 1
     for i, lists in enumerate(board):
         for a in range(0,len(lists)):
-            lists[a] = 0 if lists[a]=='X' else 1
+            lists[a] = 0 if lists[a]==0 else 1
             
     for i in range(1,len(board)):
         for j in range(1,len(board[i])):
@@ -10,8 +15,3 @@ def findLargestSquare(board):
                 board[i][j] = min(board[i-1][j],board[i][j-1],board[i-1][j-1]) +1
                 answer = answer if answer > board[i][j] else board[i][j]
     return pow(answer,2)
-
-#아래 코드는 출력을 위한 테스트 코드입니다.
-
-testBoard = [['X','O','O','O','X'],['X','O','O','O','O'],['X','X','O','O','O'],['X','X','O','O','O'],['X','X','X','X','X']]
-print(findLargestSquare(testBoard))
