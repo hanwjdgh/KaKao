b52521d19994a887018291ab28a8258157d00e92
hanwjdgh <hanwjdgh@naver.com>
2018-07-08 15:12:31 +0900

    python level 3 - Long jump

--- Python/level 3/Long jump.py
+++ Python/level 3/Long jump.py
@@ -1,8 +1,5 @@
-def jumpCase(num):
-    a, b = 1, 2
-    for i in range(2,num):
+def solution(num):
+    a, b = 1, 1
+    for i in range(1,num):
         a, b = b, a+b
-    return b
-
-#아래는 테스트로 출력해 보기 위한 코드입니다.
-print(jumpCase(4))
+    return b%1234567
