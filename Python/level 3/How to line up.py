aa505514466aef09162e92c48e12f817761f8a32
hanwjdgh <hanwjdgh@naver.com>
2018-07-09 08:04:03 +0900

    python level 3 - How to line up

--- Python/level 3/How to line up.py
+++ Python/level 3/How to line up.py
@@ -1,5 +1,5 @@
 import math
-def setAlign(n, k):
+def solution(n, k):
     list = [i for i in range(1,n+1)]
     answer = []
     for j in range(1,n+1):
@@ -8,7 +8,3 @@ def setAlign(n, k):
         answer.append(list.pop((k-1) // fac))
         n, k = n-1, k%fac
     return answer
-
-
-# 아래는 테스트로 출력해 보기 위한 코드입니다.
-print(setAlign(5, 24))
