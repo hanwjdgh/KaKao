06550700172c1b8b2009c17e161947af81c24325
hanwjdgh <hanwjdgh@naver.com>
2018-07-10 07:52:01 +0900

    python level 3 - Best set

--- Python/level 3/Best set.py
+++ Python/level 3/Best set.py
@@ -1,4 +1,4 @@
-def bestSet(n, s):
+def solution(n, s):
     answer = [-1]
     result = [int(s/n) for i in range(0,n)]
     if n > s:
@@ -7,6 +7,3 @@ def bestSet(n, s):
         result[i%n] += 1
     result.sort()
     return result
-
-# 아래는 테스트로 출력해 보기 위한 코드입니다.
-print(bestSet(3,13))
