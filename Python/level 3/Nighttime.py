82c099884b97b11a1b57cefd34051b3dee6cdb40
hanwjdgh <hanwjdgh@naver.com>
2018-09-02 08:57:21 +0900

    Nighttime

--- Python/level 3/Nighttime.py
+++ Python/level 3/Nighttime.py
@@ -1,6 +1,17 @@
-def noOvertime(n, works):
-    # 야근 지수를 최소화 하였을 때의 야근 지수는 몇일까요?
+def solution(n, works):
+    if n >= sum(works):
+        return 0
+    works = sorted(works)
+    print(works)
     for i in range(0,n):
-        works[works.index(max(works))] -= 1
+        for j in range(len(works)-1,-1,-1):
+            print(works[j],works[j-1])
+            if works[j] > works[j-1]:
+                break
+        print(j)
+        works[j] -= 1
+
+        print(works)
     return sum([i*i for i in works])
-    
+
+solution(4,[4,3,3])
