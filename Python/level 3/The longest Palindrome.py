f370ddb55cce728ee98495044eca51ee9f87cf35
hanwjdgh <hanwjdgh@naver.com>
2018-09-03 08:41:05 +0900

    python level 3 - The longest Palindrome

--- Python/level 3/The longest Palindrome.py
+++ Python/level 3/The longest Palindrome.py
@@ -3,6 +3,7 @@ def solution(s):
     n = len(T)
     P = [0] * n
     C = R = 0
+    
     for i in range (1, n-1):
         P[i] = (R > i) and min(R - i, P[2*C - i]) 
         while T[i + 1 + P[i]] == T[i - 1 - P[i]]:
